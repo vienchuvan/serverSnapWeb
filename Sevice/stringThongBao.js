f5fb37c03670f1a68753b075422c61b1de396379
vienit <vienchelsea325@gmail.com>
2025-03-26 23:21:15 +0700

    vienday

--- Sevice/stringThongBao.js
+++ Sevice/stringThongBao.js
@@ -1,4 +1,7 @@
 
-export const ThongBao = {
-    ThanhCong : "Thành công",
+export const thongBao = {
+    messThanhCong : "Thành công",
+    messThatBai : "Thất bại, vui lòng thử lại" ,
+    messLoiServer :"Lỗi hệ thông vui lòng thử lại !",
+    messThieuDuLieu : "Thiếu dữ liệu đầu vào",
 }
--- controller/quanTriBaiViet.js
+++ controller/quanTriBaiViet.js
@@ -7,6 +7,7 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs"); // 🔥 Thêm module fs để kiểm tra thư mục
 const { thongBao } = require("../Sevice/stringThongBao");
+const e = require("cors");
 
 // Đảm bảo thư mục uploads tồn tại
 const app = express();
@@ -44,36 +45,86 @@ router.post("/upload", upload.single("image"), (req, res) => {
 router.use("/uploads", express.static(uploadDir));
 
 
-router.post("/services/apiBaiViet ", async (req, res )=>{
-  try{
-    const { funcId,  user, title, shortContent, content, urlImgBaiViet, shortUrl } =
-    req.body;
-   
-    if ( !funcId||!user || !title || !shortContent || !content || !urlImgBaiViet || !shortUrl) {
-      return res.status(400).json({ error: "Thiếu dữ liệu đầu vào" });
+router.post("/services/apiBaiViet", async (req, res) => {
+  try {
+    const { funcId, user, title, shortContent, content, urlImgBaiViet, shortUrl, id } =
+      req.body;
+      if (funcId === 5 || funcId === 6) {
+    if (!funcId || !user || !title || !shortContent || !content || !urlImgBaiViet || !shortUrl) {
+      return res.status(400).json({ error: thongBao.messThieuDuLieu });
     }
+    const titleMaHoa = Buffer.from(title, 'utf-8').toString('base64')
+    const shortContentMaHoa = Buffer.from(shortContent, 'utf8').toString('base64')
+    const contentMaHoa = Buffer.from(content, 'utf-8').toString('base64')
 
-     title =  Buffer.from(title, 'utf-8').toString('base64')
-    shortContent =  Buffer.from(shortContent, 'utf8').toString('base64')
-    content  =  Buffer.from(content, 'utf-8').toString('base64')
-
-    db.query("UPDATE quantri SET title = ?, shortContent = ?,content= ? ,urlImgBaiViet = ? ,shortUrl = ? "),[
-      user,
-      title,
-      shortContent,
-      content,
-      urlImgBaiViet,
-      shortUrl,
-    ], (err , result)=>{
-      if(err){
-        return res.status(404).json({ err:"Lỗi update vui lòng thử lại"})
-      }else{
-        return res.status(200).json({ message: " Cập nhật dữ liệu thành công "})
+    if (funcId === 5) { // api update bai viet
+      if (!id) {
+        return res.status(400).json({ error: thongBao.messThieuDuLieu });
       }
+      const idNumber = Number(id);
+
+      db.query("UPDATE quantri SET user=?, title = ?, shortContent = ?, content = ?, urlImgBaiViet = ?, shortURL = ? WHERE id = ?",
+        [
+          user,
+          titleMaHoa,
+          shortContentMaHoa,
+          contentMaHoa,
+          urlImgBaiViet,
+          shortUrl, idNumber,
+        ], (err, result) => {
+          if (err) {
+            return res.status(404).json({ err: "Lỗi update vui lòng thử lại" })
+          } else {
+            return res.status(200).json({ message: "Cập nhật dữ liệu thành công " })
+          }
+        });
     }
-  }catch(err){
+
+    else if (funcId === 6) { //api them bai viet
+      db.query(
+        "INSERT INTO quantri(user,title,shortContent,content,urlImgBaiViet,shortURL) VALUES (?,?,?,?,?,?)",
+        [
+          user,
+          titleMaHoa,
+          shortContentMaHoa,
+          contentMaHoa,
+          urlImgBaiViet,
+          shortUrl,
+        ],
+        (err, result) => {
+          if (err) {
+            return res.status(404).json({ erro: thongBao.messThatBai });
+          }
+          return res.status(200).json({ message: thongBao.messThanhCong, result });
+
+        }
+      );
+    }
+  }
+    else if(funcId === 7){
+      console.log("vao day");
+      
+      if (!id) {
+       console.log("id ", id);
+       
+        return res.status(400).json({ error: thongBao.messThieuDuLieu });
+      }
+      const idNumber = Number(id);
+      db.query("DELETE FROM quantri WHERE id = ? " , [
+idNumber
+      ], (err, result)=>{
+        if (err) {
+          return res.status(404).json({ err: "Lỗi khi xóa vui lòng thử lại" })
+        } else {
+          return res.status(200).json({ message: thongBao.messThanhCong })
+        }
+      })
+    } else{
+      return res.status(404).json({ err: thongBao.messThieuDuLieu })
+    }
+  } catch (err) {
     console.log(err);
-    
+
   }
 })
 
@@ -82,13 +133,13 @@ router.post("/services/postBaiViet", async (req, res) => {
   try {
     const { user, title, shortContent, content, urlImgBaiViet, shortUrl } =
       req.body;
-     
-      if (!user || !title || !shortContent || !content || !urlImgBaiViet || !shortUrl) {
-        return res.status(400).json({ error: "Thiếu dữ liệu đầu vào" });
-      }
-    const titleMaHoa =  Buffer.from(title, 'utf-8').toString('base64')
-    const shortContentMaHoa =  Buffer.from(shortContent, 'utf8').toString('base64')
-    const contentMaHoa =  Buffer.from(content, 'utf-8').toString('base64')
+
+    if (!user || !title || !shortContent || !content || !urlImgBaiViet || !shortUrl) {
+      return res.status(400).json({ error: "Thiếu dữ liệu đầu vào" });
+    }
+    const titleMaHoa = Buffer.from(title, 'utf-8').toString('base64')
+    const shortContentMaHoa = Buffer.from(shortContent, 'utf8').toString('base64')
+    const contentMaHoa = Buffer.from(content, 'utf-8').toString('base64')
     db.query(
       "INSERT INTO quantri(user,title,shortContent,content,urlImgBaiViet,shortUrl) VALUES (?,?,?,?,?,?)",
       [
@@ -104,23 +155,23 @@ router.post("/services/postBaiViet", async (req, res) => {
           console.error("Lỗi đăng bài ", err);
           return res.status(404).json({ erro: thongBao.messThatBai });
         }
-          return res.status(200).json({ message: thongBao.messThanhCong, result });
-     
+        return res.status(200).json({ message: thongBao.messThanhCong, result });
+
       }
     );
   } catch (err) {
     console.log(err);
-    return res.status(500).json({ error: thongBao.messLoiServer});
+    return res.status(500).json({ error: thongBao.messLoiServer });
   }
 });
-router.get("/services/getBaiViet", async (req, res )=>{
-  db.query("SELECT * FROM quantri", (err, result) =>{
-    if(err){
+router.get("/services/getBaiViet", async (req, res) => {
+  db.query("SELECT * FROM quantri", (err, result) => {
+    if (err) {
       console.log('Lỗi lấy bài viết, vui lòng thử lại sau');
-      return res.status(404).json({error: "Lỗi lấy bài viết, vui lòng thử lại sau !"})
-      
+      return res.status(404).json({ error: "Lỗi lấy bài viết, vui lòng thử lại sau !" })
+
     }
-    return res.status(200).json({response:result})
+    return res.status(200).json({ response: result })
   })
 })
 module.exports = router;
